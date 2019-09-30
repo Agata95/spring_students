567b86637f17c541ed3cf462f966115fa4da48b0
Agata <agata.weltrowska@outlook.com>
2019-09-30 19:54:45 +0200

    indexController done

--- src/main/java/com/javagda25/spring/students/controller/IndexController.java
+++ src/main/java/com/javagda25/spring/students/controller/IndexController.java
@@ -1,14 +1,39 @@
 package com.javagda25.spring.students.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(path = "/demo/")
 public class IndexController {
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String wyswietlMojaStroneHelloWorldTySpringu(){
+    public String wyswietlMojaStroneHelloWorldTySpringu() {
         return "strona-hello";
+    }
+
+    //    alternatywa dla RequestMapping:
+    @GetMapping(path = "/hello")
+    public String wyswietlHello() {
+        return "strona-hello";
+    }
+
+//    klasa Model - przekazuje dane z kontrolera do widoku (na stronę html)
+    @GetMapping(path = "/helloTo") // przykład: helloTo?imie=Agata
+    public String wyswietlHelloTo(Model model,
+                                  @RequestParam(name = "imie", required = false) String parametrImie) {
+        model.addAttribute("atrybutImie", parametrImie);
+
+        return "strona-hello-to";
+    }
+
+//    drugi sposób przekazania parametru do aplikacji
+    @GetMapping("/helloMyBaby/{babyName}") // przykład: helloMyBaby/Agata
+    public String wyswietlHelloToBaby(Model model,
+                                  @PathVariable(name = "babyName") String variable) {
+        model.addAttribute("atrybutImie", variable);
+
+        return "strona-hello-to";
     }
 }
